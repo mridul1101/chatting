395553f3b3ee3fec2f1a659b48eb3f374da813b1
mridul1101 <84969910+mridul1101@users.noreply.github.com>
2021-12-28 19:46:36 +0530

    Add files via upload

---
+++ chatting application/src/chatting/application/classLoader.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatting.application;
+
+/**
+ *
+ * @author mridulsharma
+ */
+class classLoader {
+
+    static String getSystemResource() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static String getSystemResource(String chattingapplicationicons3png) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
